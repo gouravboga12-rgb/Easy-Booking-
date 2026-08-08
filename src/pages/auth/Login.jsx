d61b48f010d9d627f8d956406996cba6424f0ebc
gouravboga12-rgb <gouravboga12@gmail.com>
2026-08-08 17:47:50 +0530

    Fix missing useEffect import in Login.jsx causing white screen on Customer Login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../../store/useAuthStore';
 import { useGoogleLogin } from '@react-oauth/google';
